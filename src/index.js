e1deb73d02bee443f0b4a7f73cc2105aaad76f1d
Max Schoening <max@max.dev>
2019-10-01 11:37:20 -0700

    Try things

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
+const github = require("@actions/github");
+
 const run = async () => {
   console.log("->> Starting Prettier...");
+  const files = await github.pulls.listFiles({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: 1
+  });
+
+  console.log(files);
 };
 
 module.exports = run;
