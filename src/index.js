6071f21ad68670fa03f5b0acb8ce7ed99656b279
Max Schoening <max@max.dev>
2019-10-01 17:36:44 -0700

    Simpler

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-// const core = require("@actions/core");
 const github = require("@actions/github");
 
 const run = async () => {
   const client = new github.GitHub(process.env.GITHUB_TOKEN);
 
-  let files;
-  try {
-    files = await client.pulls.listFiles({
-      owner: github.context.repo.owner,
-      repo: github.context.repo.repo,
-      pull_number: github.context.issue.number
-    });
-  } catch (err) {
-    console.log(err);
-    process.exit(1);
-  }
+  const { data: changedFiles } = await client.pulls.listFiles({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    pull_number: github.context.issue.number
+  });
 
-  files.data.map(({ filename }) => console.log(filename));
+  changedFiles.map(({ filename }) => console.log(filename));
 };
 
 module.exports = run;
